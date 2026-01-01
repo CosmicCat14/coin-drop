db121ca906a7493da85f071c06832fbd4a4e1226
CosmicCat14 <cosmiccatnebula@icloud.com>
2026-01-01 15:29:22 -0600

    Add initial Coin Drop HTML structure

---
+++ index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+  <head>
+    <title>Coin Drop</title>
+  </head>
+  <body>
+    <h1>Play Coin Drop!</h1>
+    <button onclick="location = 'coin-drop.html';">Go!</button>
+  </body>
+</html>
